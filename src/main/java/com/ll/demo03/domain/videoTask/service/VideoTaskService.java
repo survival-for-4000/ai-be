64aaac5d70f1ec5f194681e147f72bb41e93380e
Yoolim Han <ninetale22@gmail.com>
2025-04-01 16:05:18 +0900

    add: 업스케일링과 동영상 도입/ front.hoit로 리다이렉트 url 변경

--- src/main/java/com/ll/demo03/domain/videoTask/service/VideoTaskService.java
+++ src/main/java/com/ll/demo03/domain/videoTask/service/VideoTaskService.java
@@ -1,69 +1,67 @@
-package com.ll.demo03.domain.task.service;
+package com.ll.demo03.domain.videoTask.service;
 
 import com.ll.demo03.domain.image.entity.Image;
 import com.ll.demo03.domain.image.repository.ImageRepository;
-import com.ll.demo03.domain.referenceImage.service.ReferenceImageService;
 import com.ll.demo03.domain.sse.repository.SseEmitterRepository;
 import com.ll.demo03.domain.task.dto.AckInfo;
-import com.ll.demo03.domain.task.dto.ImageUrlsResponse;
-import com.ll.demo03.domain.task.dto.WebhookEvent;
-import com.ll.demo03.domain.task.entity.Task;
-import com.ll.demo03.domain.task.repository.TaskRepository;
+import com.ll.demo03.domain.upscaledTask.dto.UpscaleImageUrlResponse;
+import com.ll.demo03.domain.videoTask.dto.VideoWebhookEvent;
+import com.ll.demo03.domain.videoTask.entity.VideoTask;
+import com.ll.demo03.domain.videoTask.repository.VideoTaskRepository;
 import com.ll.demo03.global.error.ErrorCode;
 import com.ll.demo03.global.exception.CustomException;
+import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import jakarta.persistence.EntityNotFoundException;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import com.mashape.unirest.http.HttpResponse;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-import software.amazon.awssdk.services.s3.S3Client;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
-@RequiredArgsConstructor
-@Transactional
 @Slf4j
-public class TaskService {
+@RequiredArgsConstructor
+public class VideoTaskService {
+
+    private final VideoTaskRepository videoTaskRepository;
+    private final Map<String, AckInfo> pendingAcks = new ConcurrentHashMap<>();
+    private final ImageRepository imageRepository;
+    private final SseEmitterRepository sseEmitterRepository;
 
     @Value("${piapi.api.key}")
     private String piApiKey;
 
-    @Value("${r2.bucket}")
-    private String bucket;
-
-    private final Map<String, AckInfo> pendingAcks = new ConcurrentHashMap<>();
-    private final TaskRepository taskRepository;
-    private final ImageRepository imageRepository;
-    private final SseEmitterRepository sseEmitterRepository;
-
-    public String createImage(String prompt, String ratio, String cref, String sref,  String webhook) {
+    public String createVideo( String imageUrl, String prompt,  String webhook) {
         try {
-            System.out.println("prompt: " + prompt);
-            if(sref !=null ){
-                prompt = prompt + " --sref " + sref;
-            }
-            if(cref !=null ){
-                prompt = prompt + " --cref " + cref;
-            }
+
+            Unirest.setTimeouts(0, 0);
             Unirest.setTimeouts(0, 0);
             HttpResponse<String> response = (HttpResponse<String>) Unirest.post("https://api.piapi.ai/api/v1/task")
                     .header("x-api-key", piApiKey)
                     .header("Content-Type", "application/json")
-                    .body("{\r\n    \"model\": \"midjourney\",\r\n    \"task_type\": \"imagine\",\r\n    \"input\": {\r\n        \"prompt\": \"" + prompt + "\",\r\n        \"aspect_ratio\": \"" + ratio + "\",\r\n        \"process_mode\": \"fast\",\r\n        \"skip_prompt_check\": false,\r\n        \"bot_id\": 0\r\n    },\r\n    \"config\": {\r\n        \"service_mode\": \"\",\r\n        \"webhook_config\": {\r\n            \"endpoint\": \"" + webhook+ "\",\r\n            \"secret\": \"123456\"\r\n        }\r\n    }\r\n}")
+                    .body("{\n" +
+                            "    \"model\": \"hailuo\",\n" +
+                            "    \"task_type\": \"video_generation\",\n" +
+                            "    \"input\": {\n" +
+                            "        \"prompt\": \"" + prompt + "\",\n" +
+                            "        \"model\": \"i2v-01\",\n" +
+                            "        \"image_url\": \"" + imageUrl + "\",\n" +
+                            "        \"expand_prompt\": true\n" +
+                            "    },\n" +
+                            "    \"config\": {\n" +
+                            "        \"service_mode\": \"public\",\n" +
+                            "        \"webhook_config\": {\n" +
+                            "            \"endpoint\": \"" + webhook + "\",\n" +
+                            "            \"secret\": \"123456\"\n" +
+                            "        }\n" +
+                            "    }\n" +
+                            "}")
                     .asString();
 
             return response.getBody();
@@ -73,8 +71,8 @@ public class TaskService {
         }
     }
 
-    public void processWebhookEvent(WebhookEvent event) {
-        log.info("웹훅 이벤트 수신: {}", event.getData().getTask_id());
+    public void processWebhookEvent(VideoWebhookEvent event) {
+        log.info("동영상 웹훅 이벤트 수신: {}", event.getData().getTaskId());
 
         try {
             if (!"completed".equals(event.getData().getStatus())) {
@@ -82,43 +80,40 @@ public class TaskService {
                 return;
             }
 
-            String taskId = event.getData().getTask_id();
+            String taskId = event.getData().getTaskId();
+            String videoUrl = event.getData().getOutput().getDownloadUrl();
 
-            List<String> imageUrls = event.getData().getOutput().getImage_urls();
-
-            if (imageUrls == null || imageUrls.isEmpty()) {
-                log.info("이미지 URL이 아직 생성되지 않았습니다: {}", taskId);
+            if (videoUrl == null || videoUrl.isEmpty()) {
+                log.info("동영상 URL이 아직 생성되지 않았습니다: {}", taskId);
                 return;
             }
 
-            Task task = taskRepository.findByTaskId(taskId)
-                    .orElseThrow(() -> new EntityNotFoundException("Task not found"));
+            VideoTask videoTask = videoTaskRepository.findByTaskId(taskId)
+                    .orElseThrow(() -> new EntityNotFoundException("Video task not found"));
 
-            if (imageUrls != null && imageUrls.size() >= 4) {
-                List<String> firstFourUrls = imageUrls.subList(0, 4);
-                for (int i = 0; i < firstFourUrls.size(); i++) {
-                    Image image = Image.of(firstFourUrls.get(i), task);
-                    image.setImgIndex(i + 1);
-                    imageRepository.save(image);
-                }
-            }
 
-            Long memberId = task.getMember().getId();
+            Image image = Image.ofVideo(videoUrl, videoTask);
+            image.setImgIndex(0);
+            imageRepository.save(image);
+
+            Long memberId = videoTask.getMember().getId();
             String memberIdStr = String.valueOf(memberId);
 
             SseEmitter emitter = sseEmitterRepository.get(memberIdStr);
 
             if (emitter != null) {
-                ImageUrlsResponse response = new ImageUrlsResponse(imageUrls, taskId);
+                UpscaleImageUrlResponse response = new UpscaleImageUrlResponse(videoUrl, taskId);
 
                 try {
                     emitter.send(SseEmitter.event()
                             .name("result")
                             .data(response));
 
-                    log.info("클라이언트에게 이미지 URL 전송 완료: {}, memberId: {}", taskId, memberId);
+                    log.info("클라이언트에게 동영상 이미지 URL 전송 완료: {}, memberId: {}", taskId, memberId);
+
                     acknowledgeTask(taskId);
 
+                    emitter.complete();
                     sseEmitterRepository.removeTaskMapping(taskId);
                 } catch (Exception e) {
                     log.error("SSE 이벤트 전송 중 오류: {}", e.getMessage(), e);
@@ -128,7 +123,7 @@ public class TaskService {
                 log.warn("해당 사용자 ID에 대한 SSE 연결이 없습니다: {}, taskId: {}", memberId, taskId);
             }
         } catch (Exception e) {
-            log.error("웹훅 이벤트 처리 중 오류 발생: {}", e.getMessage(), e);
+            log.error("동영상 웹훅 이벤트 처리 중 오류 발생: {}", e.getMessage(), e);
         }
     }
 
